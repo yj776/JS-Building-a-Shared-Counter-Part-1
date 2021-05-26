046d3ce9d5eca42e0510b853df1e65bb9c21aec3
Joshua Miles <joshua.m@hackerusa.com>
2021-05-26 10:42:15 -0500

    solution code complete

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    const response = await fetch('http://localhost:9000/api/counter');
+
+    const result = await response.json();
+    
+    let countValue = result.value;
 
     function increment(){
         countValue++;
