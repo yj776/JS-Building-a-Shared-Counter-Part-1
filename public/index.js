e5db9b589919ddaafcf6718fe05834645bdbe0b9
Joshua Miles <joshua.m@hackerusa.com>
2021-05-26 10:55:09 -0500

    accounted for dev server changes

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ async function main(){
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    const response = await fetch('http://localhost:9000/api/counter');
+    const response = await fetch('http://localhost:9001/counter');
 
     const result = await response.json();
     
